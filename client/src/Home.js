d7d779911a031de6ef7efdca3768e44cc35d4400
Aryan <Aryans8791@gmail.com>
2022-09-03 13:52:46 +0530

    invite code db

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react'
-
+import emailjs from '@emailjs/browser'
+import axios from 'axios'
 function Home() {
     const [username, setUsername] = useState()
-    const handleSubmit = (e) =>{
+    const [code, setCode] = useState()
+    const generateCode = () =>{
+      let newCode = Math.floor(Math.random() * 899999) + 100000 
+      setCode(newCode)
+    }
+    const handleSubmit = async(e) =>{
         e.preventDefault()
-        console.log(Math.floor(Math.random() * 899999) + 100000)
-        console.log(username)
+        emailjs.sendForm('service_zj8k5nm', 'template_hb84ehp', e.target, 'zzIk1iqeJsRHrp6hj')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        let data = {
+          username: username,
+          invitecode: code
+        }
+        console.log(data)
+        await axios.post('/', data)
     }
   return (<>
     <div>Enter email</div>
-    <form onSubmit={handleSubmit}>
-        <input onChange={e=>setUsername(e.target.value)} type='text' placeholder='Enter Email'></input>
+    <form onSubmit={handleSubmit} method='POST' action='/'>
+        <input onChange={e=>setUsername(e.target.value)}
+         type='text' placeholder='Enter Email' name='username'></input>
+        <button type='button' onClick={()=>generateCode()}>Generate Code</button>
+        <input 
+         type='text' placeholder='Enter Email' name="invitecode"
+         value={code}
+         ></input>
         <input type='submit' value='Send Email'></input>
     </form>
     </>
