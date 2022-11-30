f3dfd7ba1593521ca6dd0ff3ec7e0d8e03b8e935
Mohd Shahid Khan <khan42290@gmail.com>
2022-11-30 13:20:31 +0530

    refactor

--- server/src/connection/connection.js
+++ server/src/connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/stabniluser"
-    // "mongodb+srv://dbUser:Password@cluster0.g8vz6t9.mongodb.net/?retryWrites=true&w=majority"
+   
+     "mongodb+srv://dbUser:Password@cluster0.g8vz6t9.mongodb.net/?retryWrites=true&w=majority"
 , {
     useNewUrlParser:true,
     useUnifiedTopology: true
